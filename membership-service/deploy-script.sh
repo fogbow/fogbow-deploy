1488bab1f830c9b141b125018a4dee5bad9d6dd7
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-06-05 13:48:49 -0300

    Adding membership-service Dockerfile and deployment script

---
+++ membership-service/deploy-script.sh
@@ -0,0 +1,27 @@
+DIR_PATH=$(pwd)
+CONTAINER_BASE_PATH="/root/fogbow-membership-service"
+PROJECT_RESOURCES_PATH="src/main/resources"
+
+LOG4J_FILE_NAME="log4j.properties"
+CONF_FILE_NAME="membership.conf"
+
+IMAGE_NAME="fogbow/membership-service:latest"
+CONTAINER_NAME="membership-service"
+
+MEMBERSHIP_HOST_PORT="8080"
+MEMBERSHIP_CONTAINER_PORT="8080"
+
+XMPP_HOST_PORT="5347"
+XMPP_CONTAINER_PORT="5347"
+
+sudo docker pull $IMAGE_NAME
+sudo docker stop $CONTAINER_NAME
+sudo docker rm $CONTAINER_NAME
+
+sudo docker run -idt --name $CONTAINER_NAME \
+	-p $MEMBERSHIP_HOST_PORT:$MEMBERSHIP_CONTAINER_PORT \
+	-p $XMPP_HOST_PORT:$XMPP_CONTAINER_PORT \
+	-v $DIR_PATH/$CONF_FILE_NAME:$CONTAINER_BASE_PATH/$CONF_FILE_NAME:ro \
+	-v $DIR_PATH/$LOG4J_FILE_NAME:$CONTAINER_BASE_PATH/$LOG4J_FILE_NAME:ro \
+	$IMAGE_NAME
+
