b1174d398a95ada6596dc3f481b2022d0ac88838
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-06-06 12:28:53 -0300

    Arranging manager core conf files in a directory

--- manager-core/env-composer.sh
+++ manager-core/env-composer.sh
@@ -5,10 +5,11 @@ CONF_FILES_DIR="conf-files"
 CONF_FILES_LIST=$(find ./$CONF_FILES_DIR | grep .conf | xargs)
 
 MANAGER_DIR="manager-core"
+mkdir -p ./$MANAGER_DIR/$CONF_FILES_DIR
 
 for conf_file_path in $CONF_FILES_LIST; do
 	conf_file_name=$(basename $conf_file_path)
 	echo "Conf file path: $conf_file_path"
 	echo "Conf file name: $conf_file_name"
-	yes | cp -f $conf_file_path ./$MANAGER_DIR/$conf_file_name
+	yes | cp -f $conf_file_path ./$MANAGER_DIR/$CONF_FILES_DIR/$conf_file_name
 done
