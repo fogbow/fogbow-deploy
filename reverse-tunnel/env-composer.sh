747958a1eb6e72ac25c8e6b8091e1069ddbad691
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-06-06 00:00:08 -0300

    Adding reverse-tunnel environment composer script

--- reverse-tunnel/env-composer.sh
+++ reverse-tunnel/env-composer.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
+DIR_BASE=$(pwd)
 
 CONF_FILE_PATH=$1
 HOST_KEY_PATTERN="host_key_path"
-
-echo "Reading $CONF_FILE_PATH"
 
 HOST_KEY_FILE_PATH=$(cat $CONF_FILE_PATH | grep $HOST_KEY_PATTERN | awk -F "=" '{print $2}')
 HOST_KEY_FILE_NAME=$(basename $HOST_KEY_FILE_PATH)
 
 echo "Host key file path: $HOST_KEY_FILE_PATH"
-echo "Host ket file name: $HOST_KEY_FILE_NAME"
+echo "Host key file name: $HOST_KEY_FILE_NAME"
 
-cp -u $HOST_KEY_FILE_PATH ./reverse-tunnel/$HOST_KEY_FILE_NAME
+REVERSE_TUNNEL_DIR="reverse-tunnel"
+REVERSE_TUNNEL_CONF_FILE="reverse-tunnel.conf"
+
+# Moving host key to deployment directory
+cp -u $HOST_KEY_FILE_PATH ./$REVERSE_TUNNEL_DIR/$HOST_KEY_FILE_NAME
+
+# Moving conf file to deployment directory
+cp -u $CONF_FILE_PATH ./$REVERSE_TUNNEL_DIR/$REVERSE_TUNNEL_CONF_FILE
+
+# Replacing keys and values in reverse-tunnel conf file
+
+# Removing comments and blank lines
+sed -i -e 's/#.*//;/^$/d' ./$REVERSE_TUNNEL_DIR/$REVERSE_TUNNEL_CONF_FILE
+
+# Replacing HOST_KEY_FILE_PATH with HOST_KEY_FILE_NAME
+sed -i "s#$HOST_KEY_FILE_PATH#$HOST_KEY_FILE_NAME#" ./$REVERSE_TUNNEL_DIR/$REVERSE_TUNNEL_CONF_FILE
+
+# Replacing reverse_tunnel_http_port with http_port
+sed -i "s#reverse_tunnel_http_port#http_port#" ./$REVERSE_TUNNEL_DIR/$REVERSE_TUNNEL_CONF_FILE
