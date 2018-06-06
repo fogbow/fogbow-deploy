fa3524c5abab7f67c0e0e1f1d21756c7f1de6e12
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-06-06 11:39:31 -0300

    Adding manager environment composer script for manager deployment

---
+++ manager-core/env-composer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONF_FILES_DIR="conf-files"
+
+CONF_FILES_LIST=$(find ./$CONF_FILES_DIR | grep .conf | xargs)
+
+MANAGER_DIR="manager-core"
+
+for conf_file_path in $CONF_FILES_LIST; do
+	conf_file_name=$(basename $conf_file_path)
+	echo "Conf file path: $conf_file_path"
+	echo "Conf file name: $conf_file_name"
+	yes | cp -f $conf_file_path ./$MANAGER_DIR/$conf_file_name
+done
