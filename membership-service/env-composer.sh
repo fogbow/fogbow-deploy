e5a258b7c382b20fe43f5b3f13c889c2dbda428a
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-06-06 11:15:00 -0300

    Adding membership-service environment composer for deployment

---
+++ membership-service/env-composer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CONF_FILE_PATH=$1
+
+MEMBERSHIP_CONF_FILE="membership.conf"
+MEMBERSHIP_DIR="membership-service"
+
+# Moving conf file to deployment directory
+yes | cp -rf $CONF_FILE_PATH ./$MEMBERSHIP_DIR/$MEMBERSHIP_CONF_FILE
+
