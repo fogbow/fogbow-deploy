b1445d028c6408a02db30367c953bd94909f47de
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-06-07 11:42:03 -0300

    Updating manager core deployment script

--- manager-core/deploy-script.sh
+++ manager-core/deploy-script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 DIR_PATH=$(pwd)
+CONF_FILES_DIR="conf-files"
 CONTAINER_BASE_PATH="/root/fogbow-manager-core"
+CONTAINER_CONF_FILES_DIR=".fogbow"
 
 LOG4J_FILE_NAME="log4j.properties"
 
@@ -15,7 +17,6 @@ sudo docker rm $CONTAINER_NAME
 # We are using the host network because is necessary to expose thousands of ports for the reverse tunnel, and for each exposed port the docker runs a process to connect the host and the container.
 sudo docker run -idt --network host \
 	--name $CONTAINER_NAME \
-	-v $DIR_PATH/$CONF_FILE_NAME:$CONTAINER_BASE_PATH/$CONF_FILE_NAME:ro \
-	-v $DIR_PATH/$LOG4J_FILE_NAME:$CONTAINER_BASE_PATH/$LOG4J_FILE_NAME:ro \
+	--read-only -v $DIR_PATH/$CONF_FILES_DIR:$CONTAINER_BASE_PATH/$CONTAINER_CONF_FILES_DIR \
 	$IMAGE_NAME
 
