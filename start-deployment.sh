a65e0d1bf7e61e4154c3a8a8badd751e33116a2e
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-06-07 19:52:17 -0300

    Adding get cloud info

--- start-deployment.sh
+++ start-deployment.sh
@@ -9,8 +9,8 @@ function getBehavioralInfos {
 		FEDERATION_PLUGINS_DIR=$BEHAVIOR_DIR/"federation-identity"
 		DEFAULT_FEDERATION_IDENTITY="default"
 	
-		federationIdentityRequerimentsFiles=$(ls ./$FEDERATION_PLUGINS_DIR)
-		federationIdentityTypes=$(echo "$federationIdentityRequerimentsFiles" | awk -F "-" '{print $1}')
+		requerimentsFiles=$(ls ./$FEDERATION_PLUGINS_DIR)
+		federationIdentityTypes=$(echo "$requerimentsFiles" | awk -F "-" '{print $1}')
 	
 		echo "Federation identity types: $federationIdentityTypes"
 
@@ -26,11 +26,11 @@ function getBehavioralInfos {
 		
 		echo "Federation identity type: ${federationIdentityProperties[name]}"
 		
-		federationIdentityRequerimentsFile=$(echo "$federationIdentityRequerimentsFiles" | grep "^${federationIdentityProperties[name]}-")
+		requerimentsFile=$(echo "$requerimentsFiles" | grep "^${federationIdentityProperties[name]}-")
 		
 		function getRequirements {
-			federationIdentityRequeriments=$(cat $FEDERATION_PLUGINS_DIR/$federationIdentityRequerimentsFile | grep "=" | awk -F "=" '{print $1}')
-			for requirement in $federationIdentityRequeriments; do
+			requeriments=$(cat $FEDERATION_PLUGINS_DIR/$requerimentsFile | grep "=" | awk -F "=" '{print $1}')
+			for requirement in $requeriments; do
 				read -p "$requirement=" federationIdentityProperties[$requirement]
 				echo "$requirement=${federationIdentityProperties[$requirement]}"
 			done
@@ -40,8 +40,8 @@ function getBehavioralInfos {
 			echo "Getting default values"
 		}
 		
-		if [ -n "$federationIdentityRequerimentsFile" ]; then
-			echo "Requirements file: $federationIdentityRequerimentsFile"
+		if [ -n "$requerimentsFile" ]; then
+			echo "Requirements file: $requerimentsFile"
 			getRequirements
 			getDefaultValues
 		fi
@@ -51,8 +51,8 @@ function getBehavioralInfos {
 		LOCAL_USER_CREDENTIALS_MAPPER_DIR=$BEHAVIOR_DIR/"local-user-credentials-mapper"
 		DEFAULT_LOCAL_USER_CREDENTIALS_MAPPER="default_mapper"
 	
-		localUserCredentialsMapperRequerimentsFiles=$(ls ./$LOCAL_USER_CREDENTIALS_MAPPER_DIR)
-		localUserCredentialsMapperTypes=$(echo "$localUserCredentialsMapperRequerimentsFiles" | awk -F "-" '{print $1}')
+		requerimentsFiles=$(ls ./$LOCAL_USER_CREDENTIALS_MAPPER_DIR)
+		localUserCredentialsMapperTypes=$(echo "$requerimentsFiles" | awk -F "-" '{print $1}')
 	
 		echo "Local user credentials mapper types: $localUserCredentialsMapperTypes"
 
@@ -68,11 +68,13 @@ function getBehavioralInfos {
 		
 		echo "Local user credentials mapper type: ${localUserCredentialsMapperProperties[name]}"
 		
-		localUserCredentialsMapperRequerimentsFile=$(echo "$localUserCredentialsMapperRequerimentsFiles" | grep "^${localUserCredentialsMapperProperties[name]}-")
+		requerimentsFile=$(echo "$requerimentsFiles" | grep "^${localUserCredentialsMapperProperties[name]}-")
+		
+		echo "Requirements file: $requerimentsFile"
 		
 		function getRequirements {
-			localUserCredentialsMapperRequeriments=$(cat $LOCAL_USER_CREDENTIALS_MAPPER_DIR/$localUserCredentialsMapperRequerimentsFile | grep "=" | awk -F "=" '{print $1}')
-			for requirement in $localUserCredentialsMapperRequeriments; do
+			requeriments=$(cat $LOCAL_USER_CREDENTIALS_MAPPER_DIR/$requerimentsFile | grep "=" | awk -F "=" '{print $1}')
+			for requirement in $requeriments; do
 				read -p "$requirement=" localUserCredentialsMapperProperties[$requirement]
 				echo "$requirement=${localUserCredentialsMapperProperties[$requirement]}"
 			done
@@ -82,7 +84,6 @@ function getBehavioralInfos {
 			echo "Getting default values"
 		}
 		
-		echo "Requirements file: $localUserCredentialsMapperRequerimentsFile"
 		getRequirements
 		getDefaultValues
 	}
@@ -92,7 +93,100 @@ function getBehavioralInfos {
 }
 
 function getCloudInfos {
+	CLOUD_DIR=$REQUIREMENTS_DIR/"cloud-plugins"
+	
+	function getLocalIdentityInfos {
+		LOCAL_PLUGINS_DIR=$CLOUD_DIR/"local-identity"
+	
+		requerimentsFiles=$(ls ./$LOCAL_PLUGINS_DIR)
+		localIdentityTypes=$(echo "$requerimentsFiles" | awk -F "-" '{print $1}')
+	
+		echo "Federation identity types: $localIdentityTypes"
 
+		declare -A localIdentityProperties
+		read -p 'Local identity: ' localIdentity
+	
+		localIdentityProperties[name]=$(echo "$localIdentityTypes" | fgrep -wx "$localIdentity")
+		if [ -z "${localIdentityProperties[name]}" ]; then
+			echo "Cannot identify local identity type, exiting..."
+			exit 101
+		fi
+		
+		echo "Local identity type: ${localIdentityProperties[name]}"
+		
+		requerimentsFile=$(echo "$requerimentsFiles" | grep "^${localIdentityProperties[name]}-")
+		
+		function getRequirements {
+			requeriments=$(cat $LOCAL_PLUGINS_DIR/$requerimentsFile | grep "=" | awk -F "=" '{print $1}')
+			for requirement in $requeriments; do
+				read -p "$requirement=" localIdentityProperties[$requirement]
+				echo "$requirement=${localIdentityProperties[$requirement]}"
+			done
+		}
+		
+		function getDefaultValues {
+			echo "Getting default values"
+		}
+		
+		if [ -n "$requerimentsFile" ]; then
+			echo "Requirements file: $requerimentsFile"
+			getRequirements
+			getDefaultValues
+		fi
+	}
+	
+	function getCloudInfos {
+		CLOUD_TYPE_DIR=$CLOUD_DIR/"resources"
+	
+		cloudTypes=$(ls ./$CLOUD_TYPE_DIR)
+	
+		echo "Cloud types: $cloudTypes"
+		read -p 'Cloud type: ' cloudType
+	
+		cloudType=$(echo "$cloudTypes" | fgrep -wx "$cloudType")
+		if [ -z "$cloudType" ]; then
+			echo "Cannot identify cloud type, exiting..."
+			exit 102
+		fi
+		
+		echo "Cloud Type: $cloudType"
+		
+		function getComputeInfos {
+			echo "Getting compute infos"
+			cloudTypeFiles=$(ls ./$CLOUD_TYPE_DIR/$cloudType)
+			
+			declare -A computePluginProperties
+			computePluginProperties[name]=$cloudType
+			requirementsFile=$(echo "$cloudTypeFiles" | grep "compute")
+			
+			if [ -z "$requirementsFile" ]; then
+				echo "Cannot identify the $cloudType compute plugin"
+				exit 103
+			fi
+			
+			function getRequirements {
+				requeriments=$(cat ./$CLOUD_TYPE_DIR/$cloudType/$requirementsFile | grep "=" | awk -F "=" '{print $1}')
+				for requirement in $requeriments; do
+					read -p "$requirement=" computePluginProperties[$requirement]
+					echo "$requirement=${computePluginProperties[$requirement]}"
+				done
+			}
+		
+			function getDefaultValues {
+				echo "Getting default values"
+			}
+			
+			echo "Requirements file: $requirementsFile"
+			getRequirements
+			getDefaultValues
+		}
+		
+		getComputeInfos
+	}
+	
+	getLocalIdentityInfos
+	getCloudInfos
 }
 
 getBehavioralInfos
+getCloudInfos
