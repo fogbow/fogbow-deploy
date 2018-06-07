b06e2736fe8c4d93b8b37251bb185958f794dc1f
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-06-07 15:16:06 -0300

    Adding fogbow dashboard dockerfile and deployment script

---
+++ services/dashboard/deploy-script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+DIR_PATH=$(pwd)
+
+IMAGE_NAME="fogbow/dashboard:latest"
+CONTAINER_NAME="dashboard"
+
+CONTAINER_BASE_PATH="root/fogbow-dashboard-core"
+CONF_FILE_NAME="local_settings.py"
+CONTAINER_CONF_FILE_PATH="openstack_dashboard/local"
+
+DASHBOARD_PORT=$1
+CONTAINER_PORT="8080"
+
+sudo docker pull $IMAGE_NAME
+sudo docker stop $CONTAINER_NAME
+sudo docker rm $CONTAINER_NAME
+
+sudo docker run -tdi --name $CONTAINER_NAME \
+	-p $DASHBOARD_PORT:$CONTAINER_PORT \
+	-v $DIR_PATH/$CONF_FILE_NAME:$CONTAINER_BASE_PATH/$CONTAINER_CONF_FILE_PATH/$CONF_FILE_NAME \
+	$IMAGE_NAME
+
