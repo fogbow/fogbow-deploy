7a4897317a4e9340e6734b1abfd66a0b77e3ec95
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-06-07 15:27:19 -0300

    Adding deployment script to map host and container ports

--- services/manager-core/deploy-script.sh
+++ services/manager-core/deploy-script.sh
@@ -8,6 +8,9 @@ CONTAINER_CONF_FILES_DIR=".fogbow"
 IMAGE_NAME="fogbow/manager-core:latest"
 CONTAINER_NAME="manager-core"
 
+MANAGER_PORT=$1
+CONTAINER_PORT="8080"
+
 sudo docker pull $IMAGE_NAME
 sudo docker stop $CONTAINER_NAME
 sudo docker rm $CONTAINER_NAME
@@ -15,6 +18,7 @@ sudo docker rm $CONTAINER_NAME
 # We are using the host network because is necessary to expose thousands of ports for the reverse tunnel, and for each exposed port the docker runs a process to connect the host and the container.
 sudo docker run -idt --network host \
 	--name $CONTAINER_NAME \
+	-p $MANAGER_PORT:$CONTAINER_PORT \
 	--read-only -v $DIR_PATH/$CONF_FILES_DIR:$CONTAINER_BASE_PATH/$CONTAINER_CONF_FILES_DIR \
 	$IMAGE_NAME
 
