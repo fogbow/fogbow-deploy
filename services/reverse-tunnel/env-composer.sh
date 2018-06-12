80f35e549b39fc87a2ba98d118a4e0de10d29458
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-06-12 13:56:10 -0300

    Updating reverse tunnel env composer

--- services/reverse-tunnel/env-composer.sh
+++ services/reverse-tunnel/env-composer.sh
@@ -5,16 +5,23 @@ CONF_FILES_DIR=$DIR_BASE/"conf-files"
 CONF_FILE_PATH=$CONF_FILES_DIR/"reverse-tunnel.conf"
 HOST_KEY_PATTERN="host_key_path"
 
-HOST_KEY_FILE_PATH=$(grep $HOST_KEY_PATTERN $CONF_FILE_PATH | awk -F "#" '{print $1}' | awk -F "=" '{print $2}')
+HOST_KEY_FILE_PATH=$(grep $HOST_KEY_PATTERN $CONF_FILE_PATH | awk -F "=" '{print $2}')
 HOST_KEY_FILE_NAME=$(basename $HOST_KEY_FILE_PATH)
+
+HOST_KEY_PATH_PATTERN="host_key_path"
 
 if [ -z "$HOST_KEY_FILE_PATH" ] || [ ! -f "$HOST_KEY_FILE_PATH" ]; then
 	echo "Cannot identify the host key file, using manager private key"
-	MANAGER_CONF_FILE=$CONF_FILES_DIR/"manager.conf"
+	
+	# Replacing HOST_KEY_FILE_PATH with an empty string
+	sed -i "s#$HOST_KEY_PATH_PATTERN=$HOST_KEY_FILE_PATH#$HOST_KEY_PATH_PATTERN=#" ./$REVERSE_TUNNEL_DIR/$REVERSE_TUNNEL_CONF_FILE
+	
+	MANAGER_CONF_FILES_DIR=$DIR_BASE/"services"/"manager-core"/"conf-files"
+	MANAGER_CONF_FILE=$MANAGER_CONF_FILES_DIR/"manager.conf"
 	
 	MANAGER_PRIVATE_KEY_PATTERN="manager_ssh_private_key_file_path"
-	HOST_KEY_FILE_PATH=$(grep $HOST_KEY_PATTERN $MANAGER_CONF_FILE | awk -F "#" '{print $1}' | awk -F "=" '{print $2}')
-	HOST_KEY_FILE_NAME=$(basename $HOST_KEY_FILE_PATH)
+	HOST_KEY_FILE_NAME=$(grep $MANAGER_PRIVATE_KEY_PATTERN $MANAGER_CONF_FILE | awk -F "=" '{print $2}')
+	HOST_KEY_FILE_PATH=$MANAGER_CONF_FILES_DIR/$HOST_KEY_FILE_NAME
 fi
 
 echo "Host key file path: $HOST_KEY_FILE_PATH"
@@ -24,18 +31,15 @@ REVERSE_TUNNEL_DIR="services/reverse-tunnel"
 REVERSE_TUNNEL_CONF_FILE="reverse-tunnel.conf"
 
 # Moving host key to deployment directory
-yes | cp -rf $HOST_KEY_FILE_PATH ./$REVERSE_TUNNEL_DIR/$HOST_KEY_FILE_NAME
+yes | cp -f $HOST_KEY_FILE_PATH ./$REVERSE_TUNNEL_DIR/$HOST_KEY_FILE_NAME
 
 # Moving conf file to deployment directory
-yes | cp -rf $CONF_FILE_PATH ./$REVERSE_TUNNEL_DIR/$REVERSE_TUNNEL_CONF_FILE
+yes | cp -f $CONF_FILE_PATH ./$REVERSE_TUNNEL_DIR/$REVERSE_TUNNEL_CONF_FILE
 
 # Replacing keys and values in reverse-tunnel conf file
 
-# Removing comments and blank lines
-sed -i -e 's/#.*//;/^$/d' ./$REVERSE_TUNNEL_DIR/$REVERSE_TUNNEL_CONF_FILE
-
 # Replacing HOST_KEY_FILE_PATH with HOST_KEY_FILE_NAME
-sed -i "s#$HOST_KEY_FILE_PATH#$HOST_KEY_FILE_NAME#" ./$REVERSE_TUNNEL_DIR/$REVERSE_TUNNEL_CONF_FILE
+sed -i "s#$HOST_KEY_PATH_PATTERN=#$HOST_KEY_PATH_PATTERN=$HOST_KEY_FILE_NAME#" ./$REVERSE_TUNNEL_DIR/$REVERSE_TUNNEL_CONF_FILE
 
 # Replacing reverse_tunnel_http_port with http_port
 sed -i "s#reverse_tunnel_http_port#http_port#" ./$REVERSE_TUNNEL_DIR/$REVERSE_TUNNEL_CONF_FILE
