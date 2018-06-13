0f2b23bfca4f0a5ed3714dce39b0780955c1fcda
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-06-13 13:11:40 -0300

    Updating dashboard host port

--- services/dashboard/deploy-script.sh
+++ services/dashboard/deploy-script.sh
@@ -8,7 +8,7 @@ CONTAINER_BASE_PATH="root/fogbow-dashboard-core"
 CONF_FILE_NAME="local_settings.py"
 CONTAINER_CONF_FILE_PATH="openstack_dashboard/local"
 
-DASHBOARD_PORT="9000"
+DASHBOARD_PORT="80"
 CONTAINER_PORT="8080"
 
 sudo docker pull $IMAGE_NAME
