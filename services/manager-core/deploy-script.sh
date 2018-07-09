77b8acee4ad0a7bbd75c0f1ce2ce6181b68597ba
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-07-09 20:07:56 -0300

    Add manager xmpp port configuration

--- services/manager-core/deploy-script.sh
+++ services/manager-core/deploy-script.sh
@@ -15,6 +15,13 @@ CONTAINER_PORT="8080"
 
 echo "Manager server port: $MANAGER_PORT"
 
+INTERCOMPONENT_CONF_FILE=$CONF_FILES_DIR/"intercomponent.conf"
+XMPP_PORT_PATTERN="xmpp_server_port"
+XMPP_PORT=$(grep $XMPP_PORT_PATTERN $INTERCOMPONENT_CONF_FILE | awk -F "=" '{print $2}')
+CONTAINER_XMPP_PORT="5327"
+
+echo "Manager xmpp port: $XMPP_PORT"
+
 sudo docker pull $IMAGE_NAME
 sudo docker stop $CONTAINER_NAME
 sudo docker rm $CONTAINER_NAME
@@ -22,6 +29,7 @@ sudo docker rm $CONTAINER_NAME
 sudo docker run -idt \
 	--name $CONTAINER_NAME \
 	-p $MANAGER_PORT:$CONTAINER_PORT \
+	-p $XMPP_PORT:CONTAINER_XMPP_PORT \
 	-v $DIR_PATH/$CONF_FILES_DIR:$CONTAINER_BASE_PATH/$CONTAINER_CONF_FILES_DIR \
 	$IMAGE_NAME
 
