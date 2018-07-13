f419f05a35641ee00913c05f248cc8b9f2596e6c
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-07-13 02:02:54 -0300

    Add log4j configuration

--- services/federated-network/deploy-script.sh
+++ services/federated-network/deploy-script.sh
@@ -9,6 +9,7 @@ IMAGE_NAME="fogbow/federated-network-service:latest"
 CONTAINER_NAME="federated-network-service"
 
 FEDNET_CONF_FILE_NAME="federated-network.conf"
+LOG4F_FILE_NAME="log4j.properties"
 
 SERVER_PORT_PATTERN="server_port"
 FEDNET_PORT=$(grep $SERVER_PORT_PATTERN $FEDNET_CONF_FILE_NAME | awk -F "=" '{print $2}')
@@ -24,6 +25,7 @@ sudo docker run -idt \
 	--name $CONTAINER_NAME \
 	-p $FEDNET_PORT:$CONTAINER_PORT \
 	-v $DIR_PATH/$FEDNET_CONF_FILE_NAME:$CONTAINER_BASE_PATH/$FEDNET_CONF_FILE_NAME \
+	-v $DIR_PATH/$LOG4F_FILE_NAME:$CONTAINER_BASE_PATH/$LOG4F_FILE_NAME \
 	-v $EXTRA_FILES_DIR:$CONTAINER_EXTRA_FILES_PATH \
 	$IMAGE_NAME
 
