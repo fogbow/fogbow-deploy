664f80069c5a661c0eb9efca1daf88d1c7f73b6e
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-07-17 00:48:52 -0300

    Mount host and container shared volume

--- services/federated-network-agent/deploy-script.sh
+++ services/federated-network-agent/deploy-script.sh
@@ -11,6 +11,15 @@ echo "Downloading agent scripts"
 wget -q https://raw.githubusercontent.com/fogbow/federated-network-service/master/bin/agent-scripts/config-create-federated-network -O config-create-federated-network
 wget -q https://raw.githubusercontent.com/fogbow/federated-network-service/master/bin/agent-scripts/config-delete-federated-network -O config-delete-federated-network
 
+# Mounting shared volume
+SHARED_VOLUME_DIR="/etc/ipsec.d"
+
+sudo mkdir -p $SHARED_VOLUME_DIR
+SHARED_VOLUME_SUBDIRS="aacerts acerts cacerts certs crls ocspcerts private reqs"
+for each in $SHARED_VOLUME_SUBDIRS; do
+	sudo mkdir -p $SHARED_VOLUME_DIR/$each
+done
+
 MANAGER_CONF_FILE="manager.conf"
 MANAGER_JDBC_PASSWORD_PROPERTY="jdbc_database_password"
 MANAGER_JDBC_PASSWORD=$(grep $MANAGER_JDBC_PASSWORD_PROPERTY $MANAGER_CONF_FILE | awk -F "=" '{print $2}')
@@ -28,6 +37,7 @@ sudo docker run -idt \
 	--privileged \
 	--name $CONTAINER_NAME \
 	-e VPN_PSK=$MANAGER_JDBC_PASSWORD \
+	-v $SHARED_VOLUME_DIR:$SHARED_VOLUME_DIR \
 	$IMAGE_NAME
 
 sudo docker cp $CONTAINER_NAME:$CONF_FILE_PATH $CONF_FILE_PATH
