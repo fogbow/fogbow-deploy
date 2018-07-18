4833a62937a73555aa9f0d6df6717183f3619e51
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-07-18 14:31:56 -0300

    Remove volume adding script to be execute inside container

--- services/federated-network-agent/deploy-script.sh
+++ services/federated-network-agent/deploy-script.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
-
-IMAGE_NAME="philplckthun/strongswan"
+DIR=$(pwd)
+IMAGE_NAME="fogbow/strongswan"
 CONTAINER_NAME="strongswan"
-
-CONF_FILE_PATH="/etc/ipsec.conf"
-
-# Download Agent scripts
-echo "Downloading agent scripts"
-
-wget -q https://raw.githubusercontent.com/fogbow/federated-network-service/master/bin/agent-scripts/config-create-federated-network -O config-create-federated-network
-wget -q https://raw.githubusercontent.com/fogbow/federated-network-service/master/bin/agent-scripts/config-delete-federated-network -O config-delete-federated-network
-
-# Mounting shared volume
-SHARED_VOLUME_DIR="/etc/ipsec.d"
-
-sudo mkdir -p $SHARED_VOLUME_DIR
-SHARED_VOLUME_SUBDIRS="aacerts acerts cacerts certs crls ocspcerts private reqs"
-for each in $SHARED_VOLUME_SUBDIRS; do
-	sudo mkdir -p $SHARED_VOLUME_DIR/$each
-done
 
 MANAGER_CONF_FILE="manager.conf"
 MANAGER_JDBC_PASSWORD_PROPERTY="jdbc_database_password"
 MANAGER_JDBC_PASSWORD=$(grep $MANAGER_JDBC_PASSWORD_PROPERTY $MANAGER_CONF_FILE | awk -F "=" '{print $2}')
 
 echo "VPN PSK: $MANAGER_JDBC_PASSWORD"
+
+CREATE_NETWORK_SCRIPT="create-federated-network"
+DELETE_NETWORK_SCRIPT="delete-federated-network"
 
 sudo docker pull $IMAGE_NAME
 sudo docker stop $CONTAINER_NAME
@@ -34,11 +20,10 @@ sudo docker run -idt \
 	-p 500:500/udp \
 	-p 4500:4500/udp \
 	-p 1701:1701/udp \
-	--privileged \
 	--name $CONTAINER_NAME \
 	-e VPN_PSK=$MANAGER_JDBC_PASSWORD \
-	-v $SHARED_VOLUME_DIR:$SHARED_VOLUME_DIR \
+	-v $DIR/$CREATE_NETWORK_SCRIPT:/$CREATE_NETWORK_SCRIPT \
+	-v $DIR/$DELETE_NETWORK_SCRIPT:/$DELETE_NETWORK_SCRIPT \
+	--privileged \
 	$IMAGE_NAME
-
-sudo docker cp $CONTAINER_NAME:$CONF_FILE_PATH $CONF_FILE_PATH
 
