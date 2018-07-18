2a05f771699a70cd329321004e27e8369e27d662
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-07-18 14:30:59 -0300

    Make the create and delete fednet script executable inside container

--- services/federated-network-agent/env-composer.sh
+++ services/federated-network-agent/env-composer.sh
@@ -11,3 +11,30 @@ echo "Copying $MANAGER_CONFIGURED_FILES_DIR/$MANAGER_CONFIGURED_FILE_NAME to $BA
 
 yes | cp -f $MANAGER_CONFIGURED_FILES_DIR/$MANAGER_CONFIGURED_FILE_NAME $BASE_DIR/$MANAGER_CONFIGURED_FILE_NAME
 
+# Download Agent scripts
+echo "Downloading agent scripts"
+
+RENAMED_CREATE_FEDNET_SCRIPT="create-federated-network"
+RENAMED_DELETE_FEDNET_SCRIPT="delete-federated-network"
+
+wget -q https://raw.githubusercontent.com/fogbow/federated-network-service/master/bin/agent-scripts/config-create-federated-network -O $BASE_DIR/$RENAMED_CREATE_FEDNET_SCRIPT
+
+wget -q https://raw.githubusercontent.com/fogbow/federated-network-service/master/bin/agent-scripts/config-delete-federated-network -O $BASE_DIR/$RENAMED_DELETE_FEDNET_SCRIPT
+
+echo "Creating create and delete script to agent docker container"
+
+cat > $BASE_DIR/config-create-federated-network <<EOF
+#!/bin/sh
+
+echo "Args: \$1 \$2 \$3 \$4"
+
+docker exec -it strongswan /bin/sh -c "bash $RENAMED_CREATE_FEDNET_SCRIPT \$1 \$2 \$3 \$4"
+EOF
+
+cat > $BASE_DIR/config-delete-federated-network <<EOF
+#!/bin/sh
+
+echo "Args: \$1 \$2 \$3 \$4"
+
+docker exec -it strongswan /bin/sh -c "bash $RENAMED_DELETE_FEDNET_SCRIPT \$1"
+EOF
