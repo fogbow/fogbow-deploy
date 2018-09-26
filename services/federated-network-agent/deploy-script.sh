9ac96a958d4b5e8c93b5e92df094bbae2d916846
Arnett Rufino <arn3tt@gmail.com>
2018-09-26 17:46:56 -0300

    Use previously generated ssh key to allow access from internal host into federated network agent

--- services/federated-network-agent/deploy-script.sh
+++ services/federated-network-agent/deploy-script.sh
@@ -10,14 +10,21 @@ HOSTS_CONF_FILE="hosts.conf"
 REMOTE_HOST_USER_PATTERN="remote_hosts_user"
 REMOTE_HOST_USER=$(grep $REMOTE_HOST_USER_PATTERN $HOSTS_CONF_FILE | awk -F "=" '{print $2}')
 
+GENERAL_CONF_FILE="general.conf"
+
+# key to provide access from internal host to dmz host
+DMZ_PUBLIC_KEY_PATTERN="dmz_public_key_file_path"
+DMZ_PUBLIC_KEY_PATH=$(grep $DMZ_PUBLIC_KEY_PATTERN $GENERAL_CONF_FILE_PATH | awk -F "=" '{print $2}')
+
+DMZ_PUBLIC_KEY=$(cat $DMZ_PUBLIC_KEY_PATH)
+
 AUTHORIZED_KEYS_FILE_PATH=/"home"/$REMOTE_HOST_USER/".ssh"/"authorized_keys"
-grep "$FOGBOW_SSH_PUBLIC_KEY" $AUTHORIZED_KEYS_FILE_PATH
+grep "$DMZ_PUBLIC_KEY" $AUTHORIZED_KEYS_FILE_PATH
 if [ "$?" -ne "0" ]; then
-	echo "Adding fogbow ssh public in authorized keys"
-	echo "$FOGBOW_SSH_PUBLIC_KEY" >> $AUTHORIZED_KEYS_FILE_PATH
+	echo "Adding dmz ssh public key in authorized keys"
+	echo "$DMZ_PUBLIC_KEY" >> $AUTHORIZED_KEYS_FILE_PATH
 fi
 
-GENERAL_CONF_FILE="general.conf"
 VPN_PASSWORD_KEY="vpn_password"
 VPN_PASSWORD=$(grep $VPN_PASSWORD_KEY $GENERAL_CONF_FILE | awk -F "=" '{print $2}')
 
