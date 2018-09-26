4020e8ab380901e0838b7a0e56b7ed39e195ef2a
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-09-26 17:54:35 -0300

    Generating ssh keys to allow access in DMZ host

--- env-composer.sh
+++ env-composer.sh
@@ -5,10 +5,14 @@ CONF_FILE_PATH=$CONF_FILES_DIR/"general.conf"
 
 PRIVATE_KEY_PROPERTY="private_key_file_path"
 PUBLIC_KEY_PROPERTY="public_key_file_path"
+DMZ_PRIVATE_KEY_PROPERTY="dmz_private_key_file_path"
+DMZ_PUBLIC_KEY_PROPERTY="dmz_public_key_file_path"
 VPN_PASSWORD_PROPERTY="vpn_password"
 XMPP_PASSWORD_PROPERTY="xmpp_password"
 PASSWORD_PROPERTY="password"
 
+DMZ_PRIVATE_KEY_PATH=$(grep $DMZ_PRIVATE_KEY_PROPERTY $CONF_FILE_PATH | awk -F "=" '{print $2}')
+DMZ_PUBLIC_KEY_PATH=$(grep $DMZ_PUBLIC_KEY_PROPERTY $CONF_FILE_PATH | awk -F "=" '{print $2}')
 PRIVATE_KEY_PATH=$(grep $PRIVATE_KEY_PROPERTY $CONF_FILE_PATH | awk -F "=" '{print $2}')
 PUBLIC_KEY_PATH=$(grep $PUBLIC_KEY_PROPERTY $CONF_FILE_PATH | awk -F "=" '{print $2}')
 VPN_PASSWORD_VALUE=$(grep $VPN_PASSWORD_PROPERTY $CONF_FILE_PATH | awk -F "=" '{print $2}')
@@ -31,6 +35,16 @@ if [ -z "${PRIVATE_KEY_PATH// }" ] || [ ! -s "${PRIVATE_KEY_PATH// }" ] || [ ! -
 
 	sed -i "s#.*$PRIVATE_KEY_PROPERTY=.*#$PRIVATE_KEY_PROPERTY=$PRIVATE_KEY_PATH#" $CONF_FILE_PATH
 	sed -i "s#.*$PUBLIC_KEY_PROPERTY=.*#$PUBLIC_KEY_PROPERTY=$PUBLIC_KEY_PATH#" $CONF_FILE_PATH
+fi
+
+if [ -z "${DMZ_PRIVATE_KEY_PATH// }" ] || [ ! -s "${DMZ_PRIVATE_KEY_PATH// }" ] || [ ! -s "${DMZ_PUBLIC_KEY_PATH// }" ] || [ ! -s "${DMZ_PUBLIC_KEY_PATH// }" ]; then
+	DMZ_PRIVATE_KEY_PATH=$DIR/"dmz-id_rsa"
+	DMZ_PUBLIC_KEY_PATH=$DIR/"dmz-id_rsa.pub"
+
+	ssh-keygen -f $DMZ_PRIVATE_KEY_PATH -t rsa -b 4096 -C "internal-communication-key" -N ""
+
+	sed -i "s#.*$DMZ_PRIVATE_KEY_PROPERTY=.*#$DMZ_PRIVATE_KEY_PROPERTY=$DMZ_PRIVATE_KEY_PATH#" $CONF_FILE_PATH
+	sed -i "s#.*$DMZ_PUBLIC_KEY_PROPERTY=.*#$DMZ_PUBLIC_KEY_PROPERTY=$DMZ_PUBLIC_KEY_PATH#" $CONF_FILE_PATH
 fi
 
 if [ -z ${PASSWORD_VALUE// } ]; then
