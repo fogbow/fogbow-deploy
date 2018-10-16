9591b26bea358da5d15c5dd009bdefe5b1832240
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-10-16 15:00:44 -0300

    Init apache container

--- services/fogbow-gui/env-composer.sh
+++ services/fogbow-gui/env-composer.sh
@@ -3,7 +3,6 @@ DIR=$(pwd)
 CONF_FILES_DIR=$DIR/"conf-files"
 BASE_DIR="services/fogbow-gui"
 FNS_CONF_FILES_DIR="ras-confs-to-fns"
-CERT_CONF_FILES_DIR="cert-confs"
 
 # Copying related conf files
 
@@ -102,51 +101,3 @@ elif [[ $AUTH_TYPE_CLASS = *"OpenStack"* ]]; then
 	}" >> $BASE_DIR/$CONF_FILE_NAME
 fi
 echo "};" >> $BASE_DIR/$CONF_FILE_NAME
-
-cat $BASE_DIR/$CONF_FILE_NAME
-
-# Resolving certification files for https
-
-echo "Copying certification files to service directory"
-CERT_CONF_FILE="certificate-files.conf"
-
-CERTIFICATE_FILE="SSL_certificate_file_path"
-CERTIFICATE_FILE_PATH=$(grep $CERTIFICATE_FILE $CONF_FILES_DIR/$CERT_CONF_FILES_DIR/$CERT_CONF_FILE | awk -F "=" '{print $2}')
-CERTIFICATE_FILE_NAME=$(basename $CERTIFICATE_FILE_PATH)
-yes | cp -f $CERTIFICATE_FILE_PATH $BASE_DIR/$CERTIFICATE_FILE_NAME
-
-CERTIFICATE_KEY_FILE="SSL_certificate_key_file_path"
-CERTIFICATE_KEY_FILE_PATH=$(grep $CERTIFICATE_KEY_FILE $CONF_FILES_DIR/$CERT_CONF_FILES_DIR/$CERT_CONF_FILE | awk -F "=" '{print $2}')
-CERTIFICATE_KEY_FILE_NAME=$(basename $CERTIFICATE_KEY_FILE_PATH)
-yes | cp -f $CERTIFICATE_KEY_FILE_PATH $BASE_DIR/$CERTIFICATE_KEY_FILE_NAME
-
-CERTIFICATE_CHAIN_FILE="SSL_certificate_chain_file_path"
-CERTIFICATE_CHAIN_FILE_PATH=$(grep $CERTIFICATE_CHAIN_FILE $CONF_FILES_DIR/$CERT_CONF_FILES_DIR/$CERT_CONF_FILE | awk -F "=" '{print $2}')
-CERTIFICATE_CHAIN_FILE_NAME=$(basename $CERTIFICATE_CHAIN_FILE_PATH)
-yes | cp -f $CERTIFICATE_CHAIN_FILE_PATH $BASE_DIR/$CERTIFICATE_CHAIN_FILE_NAME
-
-# Fill virtual host file
-
-VIRTUAL_HOST_FILE="000-default.conf"
-yes | cp -f $BASE_DIR/$VIRTUAL_HOST_FILE'.example' $BASE_DIR/$VIRTUAL_HOST_FILE
-SSL_DIR="/etc/ssl/private"
-
-CERTIFICATE_PATTERN="SSLCertificateFile"
-sed -i "s#$CERTIFICATE_PATTERN.*#$CERTIFICATE_PATTERN $SSL_DIR/$CERTIFICATE_FILE_NAME#" $BASE_DIR/$VIRTUAL_HOST_FILE
-
-CERTIFICATE_KEY_PATTERN="SSLCertificateKeyFile"
-sed -i "s#$CERTIFICATE_KEY_PATTERN.*#$CERTIFICATE_KEY_PATTERN $SSL_DIR/$CERTIFICATE_KEY_FILE_NAME#" $BASE_DIR/$VIRTUAL_HOST_FILE
-
-CERTIFICATE_CHAIN_PATTERN="SSLCertificateChainFile"
-sed -i "s#$CERTIFICATE_CHAIN_PATTERN.*#$CERTIFICATE_CHAIN_PATTERN $SSL_DIR/$CERTIFICATE_CHAIN_FILE_NAME#" $BASE_DIR/$VIRTUAL_HOST_FILE
-
-# Moving cert conf files
-
-CONF_FILES_LIST=$(find $CONF_FILES_DIR/$CERT_CONF_FILES_DIR | grep '.conf' | xargs)
-
-for conf_file_path in $CONF_FILES_LIST; do
-	conf_file_name=$(basename $conf_file_path)
-	echo "Conf file path: $conf_file_path"
-	echo "Conf file name: $conf_file_name"
-	yes | cp -f $conf_file_path ./$BASE_DIR/$conf_file_name
-done
