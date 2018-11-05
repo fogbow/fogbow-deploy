b9141ee5ded4f6d1921208b676174ffd1d3bbaf9
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-11-05 16:22:38 -0300

    Adding cloudstack config for GUI

---
+++ conf-files/gui-confs/cloudstack-api.config.js
@@ -0,0 +1,21 @@
+export const env = {
+    ras: , // Auto-generated value
+    fns: , // Auto-generated value
+    ms: , // Auto-generated value
+    local: , // Auto-generated value
+    refreshTime: 5000,
+    credentialFields: {
+        username: {
+            type: 'text',
+            label: 'User Name'
+        },
+        password: {
+            type: 'password',
+            label: 'Password'
+        },
+        domain: {
+            type: 'text',
+            label: 'Domain'
+        },
+    }
+};
