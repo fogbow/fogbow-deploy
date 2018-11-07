03ee10900e88996f437e7d93a8b755b3556a6660
Francisco Germano de Araujo Neto <chicog@taioba.lsd.ufcg.edu.br>
2018-11-07 09:11:39 -0300

    Adding Shibboleth configuration

---
+++ conf-files/gui-confs/shibboleth-api.config.js
@@ -0,0 +1,10 @@
+export const env = {
+    ras: , // Auto-generated value
+    fns: , // Auto-generated value
+    ms: , // Auto-generated value
+    local: , // Auto-generated value
+    remoteEndpoint: ,
+    refreshTime: 5000,
+    credentialFields: {
+    }
+};
