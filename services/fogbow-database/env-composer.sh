5aa70f8d7d9b83e756d83a966c14f649bdf87387
Gustavo Yamaguchi <gustavoyama@gmail.com>
2019-02-14 09:56:10 -0300

    Fix database deploy script

--- services/fogbow-database/env-composer.sh
+++ services/fogbow-database/env-composer.sh
@@ -6,5 +6,8 @@ BASE_DIR="services/fogbow-database/"
 # Copying service.conf file
 echo "Copying services.conf to service directory"
 SERVICES_FILE="services.conf"
-
 yes | cp -f $CONF_FILES_DIR/$SERVICES_FILE $BASE_DIR/$SERVICES_FILE
+
+# Copy shared file
+SHARED_INFO="shared.info"
+yes | cp -f $CONF_FILES_DIR/$SHARED_INFO $BASE_DIR/$SHARED_INFO
