9b7660744a1859fb492e311b0d99a99701d7c967
fubica <fubica@dsc.ufcg.edu.br>
2019-10-15 18:37:56 -0300

    test

--- basic-site/start-env-composers.sh
+++ basic-site/start-env-composers.sh
@@ -13,9 +13,11 @@ echo "Running basic-site/env-composer.sh"
 bash env-composer.sh
 cd ..
 
-SERVICES_LIST="$DATABASE_SERVICE_DIR $AUTHENTICATION_SERVICE_DIR $RESOURCE_ALLOCATION_SERVICE_DIR $APACHE_SERVICE_DIR"
+SERVICES_LIST="$DATABASE_SERVICE_DIR $AUTHENTICATION_SERVICE_DIR $RESOURCE_ALLOCATION_SERVICE_DIR"
 
 for service in $SERVICES_LIST; do
+    echo "current dir:" $(pwd)
 	echo "Running $service/basic-site-env-composer.sh"
 	bash $service/"basic-site-env-composer.sh"
+	echo "current dir:" $(pwd)
 done
