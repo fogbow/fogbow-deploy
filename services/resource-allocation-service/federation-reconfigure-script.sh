ce7b9fe66a256b4fb654aca5934c2c13136607ba
fubica <fubica@dsc.ufcg.edu.br>
2019-10-17 22:03:45 -0300

    Fixed reconfiguration RAS

--- services/resource-allocation-service/federation-reconfigure-script.sh
+++ services/resource-allocation-service/federation-reconfigure-script.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
+DIR=$(pwd)
+CONF_FILES_DIR=$DIR/"conf-files"
+HOSTS_CONF_FILE="hosts.conf"
 RAS_CONF_FILE_NAME="ras.conf"
-RAS_CONF_FILE_PATH="conf-files"/$RAS_CONF_FILE_NAME
+RAS_CONF_FILE_PATH=$CONF_FILES_DIR/$RAS_CONF_FILE_NAME
 CONTAINER_CONF_DIR="/root/resource-allocation-service/src/main/resources/private/"
 CONTAINER_NAME="resource-allocation-service"
+SECRETS_FILE_PATH=$CONF_FILES_DIR/"secrets"
+
+XMPP_PASSWORD_PATTERN="xmpp_password"
+XMPP_PASSWORD=$(grep $XMPP_PASSWORD_PATTERN $SECRETS_FILE_PATH | awk -F "=" '{print $2}')
+
+XMPP_HOST_IP_PATTERN="xmpp_host_ip"
+XMPP_HOST_IP=$(grep $XMPP_HOST_IP_PATTERN $CONF_FILES_DIR/$HOSTS_CONF_FILE | awk -F "=" '{print $2}')
 
 ed -s $RAS_FILE_PATH <<!
 /false
 s,false,true
+i
+xmpp_password=
+xmpp_server_ip=
+.
 w
 q
 !
+
+sed -i "s|xmpp_password=|xmpp_password=$XMPP_PASSWORD|g" $RAS_FILE_PATH
+sed -i "s|xmpp_server_ip=|xmpp_server_ip=$XMPP_HOST_IP|g" $RAS_FILE_PATH
 
 sudo docker stop $CONTAINER_NAME
 sudo docker start $CONTAINER_NAME
