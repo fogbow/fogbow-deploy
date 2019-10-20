f6cd66554f5af5f2edf828a10cac66457eab8020
fubica <fubica@dsc.ufcg.edu.br>
2019-10-20 18:37:21 -0300

    fix

--- services/xmpp-server/fns-deploy-reconfigure-script.sh
+++ services/xmpp-server/fns-deploy-reconfigure-script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 CONTAINER_NAME="xmpp-server"
 PROSODY_CFG_FILE_PATH="/etc/prosody/prosody.cfg.lua"
-TMP_PROSODY_CFG_FILE_PATH="/etc/prosody/prosody.cfg.lua.tmp"
+TMP_PROSODY_CFG_FILE_NAME="prosody.cfg.lua.tmp"
 
-sudo docker cp $CONTAINER_NAME:$PROSODY_CFG_FILE_PATH $TMP_PROSODY_CFG_FILE_PATH
-sudo chown ubuntu.ubuntu $TMP_PROSODY_CFG_FILE_PATH
+sudo docker cp $CONTAINER_NAME:$PROSODY_CFG_FILE_PATH $TMP_PROSODY_CFG_FILE_NAME
+sudo chown ubuntu.ubuntu $TMP_PROSODY_CFG_FILE_NAME
 
-ed -s $TMP_PROSODY_CFG_FILE_PATH <<!
+ed -s $TMP_PROSODY_CFG_FILE_NAME <<!
 /^Component "ras
 .,+t+
 -
@@ -15,6 +15,6 @@ w
 q
 !
 
-sudo docker cp $TMP_PROSODY_CFG_FILE_PATH $CONTAINER_NAME:PROSODY_CFG_FILE_PATH
+sudo docker cp $TMP_PROSODY_CFG_FILE_NAME $CONTAINER_NAME:PROSODY_CFG_FILE_PATH
 
-rm $TMP_PROSODY_CFG_FILE_PATH
+rm $TMP_PROSODY_CFG_FILE_NAME
