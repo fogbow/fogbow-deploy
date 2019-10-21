daa1ff3918782d9d6398e3b2edc6836a02e62eb3
fubica <fubica@dsc.ufcg.edu.br>
2019-10-20 21:34:47 -0300

    Fixes

--- scripts/gui-deploy/start-env-composers.sh
+++ scripts/gui-deploy/start-env-composers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 SERVICES_DIR_NAME="services"
-GUI_SERVICE_DIR_PATH=$SERVICES_DIR/"fogbow-gui"
+GUI_SERVICE_DIR_PATH=$SERVICES_DIR_NAME/"fogbow-gui"
 SERVICES_LIST="$GUI_SERVICE_DIR_PATH"
 
-#bash env-composer.sh
+bash env-composer.sh
 cd ../..
 
 for service in $SERVICES_LIST; do
