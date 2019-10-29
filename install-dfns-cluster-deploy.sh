a1d32d61d754f7db1d9ebc9a01bc05fce2dcbe73
fubica <fubica@dsc.ufcg.edu.br>
2019-10-29 17:08:27 -0300

    Fixes

--- install-dfns-cluster-deploy.sh
+++ install-dfns-cluster-deploy.sh
@@ -56,6 +56,8 @@ do
     echo "      atomix_dir_name: atomix" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
     echo "      onos_dir_name: onos"  >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
     echo "      utils_dir_name: utils"  >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
+    echo "      dfns_path: \"/home/{{ lookup(\'config\', \'DEFAULT_REMOTE_USER\')}}/dfns-agents\""  >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
+    echo "      remove_script_runner: bash remove-script.sh"  >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
     echo "  tasks:" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
     echo "      - name: Removing previously installed DFNS agent software (if applied) in agent-node-$i" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
     echo "        become: yes" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
@@ -67,6 +69,14 @@ do
     echo "          - \"{{ dfns_agent_dir_name }}/{{ onos_dir_name }}\"" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
     echo "          - \"{{ dfns_agent_dir_name }}/{{ utils_dir_name }}\"" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
     echo "" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
+    echo "  tasks:" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
+    echo "    - name: Removing containers in agent-node-$i" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
+    echo "      shell: \"{{ remove_script_runner }}\"" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
+    echo "      become: yes" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
+    echo "      args:" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
+    echo "        chdir: \"{{ item }}\"" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
+    echo "      with_items:" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
+    echo "        - \"{{ dfns_path }}\"" >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME >> $ANSIBLE_FILES_DIR_PATH/$YML_FILE_NAME
 done
 
 # Generate transfer-agents-software.yml
--- services/dfns-agents/atomix/deploy-script.sh
+++ services/dfns-agents/atomix/deploy-script.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-OTHERS=$(../get_others.sh $1)
+SITE_CONF_FILE_PATH="../conf-files/site.conf"
+
+DFNS_CLUSTER_PUBLIC_IPS_LIST_PATTERN="dfns_cluster_public_ips_list"
+DFNS_CLUSTER_PUBLIC_IPS_LIST=$(grep $DFNS_CLUSTER_PUBLIC_IPS_LIST_PATTERN $SITE_CONF_FILE_PATH | awk -F "=" '{print $2}')
+
+OTHERS=""
+
+for i in $DFNS_CLUSTER_PUBLIC_IPS_LIST
+    if [ $i != $1 ]; then
+        OTHERS=$OTHERS" "$i
+    fi
+do
+
+echo $OTHERS
 
 bash ./init_atomix_node.sh $1 $OTHERS
