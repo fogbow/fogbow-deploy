3e1b512e2cb1fede34189ee35d80a2dcc3094769
fubica <fubica@dsc.ufcg.edu.br>
2019-10-30 22:13:02 -0300

    FIxes

--- services/dfns-agents/install/add-public-key.sh
+++ services/dfns-agents/install/add-public-key.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# key to provide access from basic-site-machine to dfns-agent-machine
+# Add key to provide access from basic-site-machine to dfns-agent-machine
+
+SITE_CONF_FILE_NAME="site.conf"
+
+REMOTE_USER_PATTERN="remote_user"
+REMOTE_USER=$(grep $REMOTE_USER_PATTERN $SITE_CONF_FILE_NAME | awk -F "=" '{print $2}')
+
 AGENT_HOST_PUBLIC_KEY=$(cat dfns-agent-id_rsa.pub)
 
 AUTHORIZED_KEYS_FILE_PATH=/"home"/$REMOTE_USER/".ssh"/"authorized_keys"
