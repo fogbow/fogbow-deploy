a87b6478b56692cf48356661f9defca02db383a0
fubica <fubica@dsc.ufcg.edu.br>
2019-10-31 12:15:08 -0300

    Fixed GUI redeploy

--- services/dfns-agents/install/add-public-key.sh
+++ services/dfns-agents/install/add-public-key.sh
@@ -12,5 +12,6 @@ AGENT_HOST_PUBLIC_KEY=$(cat dfns-agent-id_rsa.pub)
 AUTHORIZED_KEYS_FILE_PATH=/"home"/$REMOTE_USER/".ssh"/"authorized_keys"
 grep "$AGENT_HOST_PUBLIC_KEY" $AUTHORIZED_KEYS_FILE_PATH
 if [ "$?" -ne "0" ]; then
-	echo "$AGENT_HOST_PUBLIC_KEY" >> $AUTHORIZED_KEYS_FILE_PATH
+        echo "" >> $AUTHORIZED_KEYS_FILE_PATH
+        echo "$AGENT_HOST_PUBLIC_KEY" >> $AUTHORIZED_KEYS_FILE_PATH
 fi
