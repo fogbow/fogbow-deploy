eef1115bdd727de6a7079f6883b24e74dae6554f
fubica <fubica@dsc.ufcg.edu.br>
2020-04-24 15:46:52 -0300

    Added first stage

---
+++ install-multi-cloud.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# This script goal is to generate the Ansible hosts and ansible.cfg files
+
+# Set path variables
+
+SITE_CONF_FILE_NAME="./conf-files/multi-cloud.conf"
+ANSIBLE_FILES_DIR_PATH="./ansible-playbook/multi-cloud"
+ANSIBLE_HOSTS_FILE_PATH=$ANSIBLE_FILES_DIR_PATH/"hosts"
+ANSIBLE_CFG_FILE_PATH=$ANSIBLE_FILES_DIR_PATH/"ansible.cfg"
+
+# Generate content of Ansible hosts file
+
+SERVICE_HOST_IP_PATTERN="service_host_ip"
+SERVICE_HOST_IP=$(grep $SERVICE_HOST_IP_PATTERN $SITE_CONF_FILE_NAME | awk -F "=" '{print $2}')
+
+SERVICE_HOST_PRIVATE_KEY_FILE_PATH_PATTERN="service_host_ssh_private_key_file"
+SERVICE_HOST_PRIVATE_KEY_FILE_PATH=$(grep $SERVICE_HOST_PRIVATE_KEY_FILE_PATH_PATTERN $SITE_CONF_FILE_NAME | awk -F "=" '{print $2}')
+
+echo "[localhost]" > $ANSIBLE_HOSTS_FILE_PATH
+echo "127.0.0.1" >> $ANSIBLE_HOSTS_FILE_PATH
+echo "" >> $ANSIBLE_HOSTS_FILE_PATH
+echo "[service-host]" >> $ANSIBLE_HOSTS_FILE_PATH
+echo $SERVICE_HOST_IP >> $ANSIBLE_HOSTS_FILE_PATH
+echo "[service-host:vars]" >> $ANSIBLE_HOSTS_FILE_PATH
+echo "ansible_ssh_private_key_file=$SERVICE_HOST_PRIVATE_KEY_FILE_PATH" >> $ANSIBLE_HOSTS_FILE_PATH
+echo "ansible_python_interpreter=/usr/bin/python3" >> $ANSIBLE_HOSTS_FILE_PATH
+
+# Generate content of Ansible ansible.cfg file
+
+REMOTE_USER_PATTERN="^remote_user"
+REMOTE_USER=$(grep $REMOTE_USER_PATTERN $SITE_CONF_FILE_NAME | awk -F "=" '{print $2}')
+
+echo "[defaults]" > $ANSIBLE_CFG_FILE_PATH
+echo "inventory = hosts" >> $ANSIBLE_CFG_FILE_PATH
+echo "remote_user = $REMOTE_USER" >> $ANSIBLE_CFG_FILE_PATH
+echo "host_key_checking = False" >> $ANSIBLE_CFG_FILE_PATH
+
+# Deploy
+
+(cd $ANSIBLE_FILES_DIR_PATH && ansible-playbook deploy.yml)
